732af25d518697fe485c33e9f3663f2571f47747
kingmacbeth <45128877+kingmacbeth@users.noreply.github.com>
2025-05-15 14:00:45 -0300

    complex number calc change plus security subject exercise added

--- complex_numbers_calculator/calculator.py
+++ complex_numbers_calculator/calculator.py
@@ -11,9 +11,10 @@ As constantes π (Pi) e ε (Euler) sao aceitas caso escritas como "pi" ou "euler
 requisitada inserção de dados pelo usuário.
 """
 
-#Constants
+# Constants
 PI = cmath.pi
 EULER = cmath.e
+
 
 class Operations():
     def __init__(self, z1):
@@ -39,12 +40,13 @@ class Operations():
         div = self.z1 / z2
         return div
 
+
 class Helper():
     def convert_to_float(user_number):
         pi_or_euler = {
             "pi": PI,
             "euler": EULER,
-            }
+        }
 
         if user_number in pi_or_euler:
             return pi_or_euler[user_number]
@@ -53,6 +55,7 @@ class Helper():
 
     def exit_calc():
         return sys.exit("Até logo!")
+
 
 def complex_z1():
     re1 = input("Digite o número real do primeiro complexo: ")
@@ -65,6 +68,7 @@ def complex_z1():
 
     return z1
 
+
 def complex_z2():
     re2 = input("Digite o número real do segundo complexo: ")
     im2 = input("Digite o número imaginario do segundo complexo: ")
@@ -76,6 +80,7 @@ def complex_z2():
 
     return z2
 
+
 def choices():
     print("")
     print("[1] Conjugado\n[2] Adição\n[3] Subtração\n[4] Multiplicação\n[5] Divisão")
@@ -84,65 +89,68 @@ def choices():
 
     return choice
 
+
 def second_choices(choice, result=None):
-        if choice != "1":
-            if result is None:
-                z2 = complex_z2()
+    if choice != "1":
+        if result is None:
+            z2 = complex_z2()
 
-            else:
-                z2 = complex_z2()
+        else:
+            z2 = complex_z2()
 
-                if choice == "2":
-                    result = operation.addition(z2)
-                    print("A soma dos complexos é: ", result)
-                    time.sleep(2)
-
-                    new_options(result)
-
-                elif choice == "3":
-                    result = operation.subtraction(z2)
-                    print("A subtração dos complexos é: ", result)
-                    time.sleep(2)
-
-                    new_options(result)
-
-                elif choice == "4":
-                    result = operation.multiplication(z2)
-                    print("A multiplicação dos complexos é: ", result)
-                    time.sleep(2)
-
-                    new_options(result)
-
-                elif choice == "5":
-                    result = operation.division(z2)
-                    print("A divisão dos complexos é: ", result)
-                    time.sleep(2)
-
-                    new_options(result)
-
-def new_options(result):
-        print("")
-        print("[1] Sair da Calculadora.\n[2] Reiniciar Calculadora.\n[3] Realizar operação com o resultado anterior.")
-        print("")
-        choice_final = input("Digite qual operação deseja utilizar: ")
-        if choice_final == "1":
-            print("Saindo...")
-            time.sleep(2)
-            Helper.exit_calc()
-        elif choice_final == "2":
-            print("Reiniciando a calculadora...")
-            time.sleep(2)
-            print("")
-        elif choice_final == "3":
-            choice = choices()
-            if choice == "1":
-                print("O conjugado é: ", result)
+            if choice == "2":
+                result = operation.addition(z2)
+                print("A soma dos complexos é: ", result)
                 time.sleep(2)
+
                 new_options(result)
 
-            else:
-                operation.z1 = result
-                second_choices(choice, result)
+            elif choice == "3":
+                result = operation.subtraction(z2)
+                print("A subtração dos complexos é: ", result)
+                time.sleep(2)
+
+                new_options(result)
+
+            elif choice == "4":
+                result = operation.multiplication(z2)
+                print("A multiplicação dos complexos é: ", result)
+                time.sleep(2)
+
+                new_options(result)
+
+            elif choice == "5":
+                result = operation.division(z2)
+                print("A divisão dos complexos é: ", result)
+                time.sleep(2)
+
+                new_options(result)
+
+
+def new_options(result):
+    print("")
+    print("[1] Sair da Calculadora.\n[2] Reiniciar Calculadora.\n[3] Realizar operação com o resultado anterior.")
+    print("")
+    choice_final = input("Digite qual operação deseja utilizar: ")
+    if choice_final == "1":
+        print("Saindo...")
+        time.sleep(2)
+        Helper.exit_calc()
+    elif choice_final == "2":
+        print("Reiniciando a calculadora...")
+        time.sleep(2)
+        print("")
+    elif choice_final == "3":
+        choice = choices()
+        if choice == "1":
+            print("O conjugado é: ", result)
+            time.sleep(2)
+            new_options(result)
+
+        else:
+            operation.z1 = result
+            second_choices(choice, result)
+
 
 my_bool = True
 
